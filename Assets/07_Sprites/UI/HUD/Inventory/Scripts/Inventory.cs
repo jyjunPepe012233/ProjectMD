eeaadc2d0acde9802bf97e6dc41ab22e981fc475
leewoojin1234 <geonui542@gmail.com>
2024-10-17 19:57:26 +0900

    feat :: Build InventoryUI , InventorySlot

---
+++ Assets/07_Sprites/UI/HUD/Inventory/Scripts/Inventory.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject slotPrefab;
+    public Transform inventoryPanel;
+    private List<InventorySlot> slots = new List<InventorySlot>();
+
+    private PlayerInventoryHandler playerInventory; 
+
+    void Start()
+    {
+        playerInventory = FindObjectOfType<Player>().inventory;
+
+        CreateSlots(25);
+        UpdateInventoryUI();
+    }
+
+    void CreateSlots(int slotCount)
+    {
+        for (int i = 0; i < slotCount; i++)
+        {
+            GameObject newSlotObject = Instantiate(slotPrefab, inventoryPanel);
+            InventorySlot slot = newSlotObject.GetComponent<InventorySlot>();
+            slots.Add(slot);
+        }
+    }
+    
+    public void UpdateInventoryUI()
+    {
+        Item[] playerItems = playerInventory.playerItemList;
+        int slotIndex = 0; 
+
+        for (int i = 0; i < playerItems.Length; i++)
+        {
+            if (playerItems[i] != null && playerItems[i].itemCount > 0)
+            {
+                if (slotIndex < slots.Count)
+                {
+                    slots[slotIndex].SetItem(playerItems[i]); 
+                    slotIndex++; 
+                }
+            }
+        }
+
+        for (int i = slotIndex; i < slots.Count; i++)
+        {
+            slots[i].ClearSlot();
+        }
+    }
+
+}
