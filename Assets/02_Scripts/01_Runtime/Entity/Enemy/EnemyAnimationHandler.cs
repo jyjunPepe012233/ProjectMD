a88cd68f35b02e2d81b31d1dfae9534adf1aa80a
jyjun_080808 <jinyj0217@gmail.com>
2024-12-20 10:37:47 +0900

    feat :: Now animation handler can checking state is exist

--- Assets/02_Scripts/01_Runtime/Entity/Enemy/EnemyAnimationHandler.cs
+++ Assets/02_Scripts/01_Runtime/Entity/Enemy/EnemyAnimationHandler.cs
@@ -1,15 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace MinD.Runtime.Entity {
 
 public class EnemyAnimationHandler : BaseEntityAnimationHandler {
-	
+
+	private Dictionary<string, bool> isStatesExist = new();
 	
 
-	public void PlayTargetAnimation(string stateName, float normalizedTransformDuration, bool applyRootMotion, bool isPerformingAction) {
-	
-		owner.animator.CrossFadeInFixedTime(stateName, normalizedTransformDuration);
+	public bool PlayTargetAnimation(string stateName, float normalizedTransformDuration, bool applyRootMotion, bool isPerformingAction) {
+
+		if (!isStatesExist.ContainsKey(stateName)) {
+			isStatesExist.Add(stateName, owner.animator.HasState(0, Animator.StringToHash(stateName)));
+		}
+
+		if (isStatesExist[stateName]) {
+			owner.animator.CrossFadeInFixedTime(stateName, normalizedTransformDuration);
+		}
+		
 		
 		owner.animator.applyRootMotion = applyRootMotion;
-		((Player)owner).isPerformingAction = isPerformingAction;
+		((Enemy)owner).isPerformingAction = isPerformingAction;
+
+		return isStatesExist[stateName];
 	}
 
 }
