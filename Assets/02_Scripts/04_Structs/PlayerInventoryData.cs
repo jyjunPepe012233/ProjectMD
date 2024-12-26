58ba24e132a63fb7531d00e5cb9d1fb01253b446
jyjun_080808 <jinyj0217@gmail.com>
2024-12-26 11:38:31 +0900

    feat :: Add magic save method

--- Assets/02_Scripts/04_Structs/PlayerInventoryData.cs
+++ Assets/02_Scripts/04_Structs/PlayerInventoryData.cs
@@ -8,19 +8,22 @@ public struct PlayerInventoryData {
 	public Protection protection;
 	public Talisman[] talismans;
 	public Tool[] tools;
+	public Magic[] magics;
 	public Item[] allItems;
 		
 	public PlayerInventoryData (
 		Weapon weapon = null,
 		Protection protection = null,
 		Talisman[] talismans = null, 
-		Tool[] tools = null, 
+		Tool[] tools = null,
+		Magic[] magics = null,
 		Item[] allItems = null)
 	{
 		this.weapon = weapon;
 		this.protection = protection;
 		this.talismans = talismans;
 		this.tools = tools;
+		this.magics = magics;
 		this.allItems = allItems;
 	}
 }
